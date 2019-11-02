2a24159091f72cde49fb7e9942bd6541208bb2ed
trigodeepak <trigodeepak@gmail.com>
2019-11-02 12:12:24 +0530

    Forms demo

--- src/App.js
+++ src/App.js
@@ -4,54 +4,120 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from 'react-dom';
 
-var course = [  {id: 1, name: "Angular", educator: "Rob"},
-                {id: 2, name: "ReactJS", educator: "John"},
-                {id: 3, name: "Ajax", educator: "Mack"}
-            ];
 
 class App extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = { courseData: course, selected: false };
-        console.log("Initialization Phase: 'constructor()' executed");
-        
+    constructor() {
+        super();
+        this.state = {
+            empId: '',
+            empName: '',
+            age: '',
+            salary: '',
+            achievements: '',
+            formErrors: {
+                empNameErr: '',
+                empAgeErr: ''
+            },
+            fieldValidity: {
+                empName: false,
+                age: false
+            },
+            formValid: false,
+            successMessage: ''
+        };
     }
-    componentWillMount = () => {
-        console.log("Mounting Phase: 'componentWillMount()' executed");  
+    validateName = (e) => {
+        const name = e.target.value;
+        var formErrors = this.state.formErrors;
+        var fieldValidity = this.state.fieldValidity;
+        this.setState({ empName: e.target.value });
+        console.log(name.length);
+        if (name.length < 6) {
+            formErrors.empNameErr = "Name must be at least 6 chars";
+            fieldValidity.empName = false;
+        }
+        else {
+            formErrors.empNameErr = "";
+            fieldValidity.empName = true;
+        }
+        this.setState({ fieldValidity: fieldValidity })
+        this.setState({ formValid: fieldValidity.empName && fieldValidity.age })
     }
-    
-    render = () => {
-        var users = this.state.courseData;
-        console.log("Mounting Phase: 'render()' executed");
+    validateAge = (e) => {
+        const age = e.target.value;
+        this.setState({ age: age });
+        var formErrors = this.state.formErrors;
+        var fieldValidity = this.state.fieldValidity;
+        if (age < 18 || age > 60) {
+            formErrors.empAgeErr = "Age must be between 18 and 60"
+            fieldValidity.age = false;
+        }
+        else {
+            formErrors.empAgeErr = ""
+            fieldValidity.age = true;
+        }
+        this.setState({ formErrors: formErrors });
+        this.setState({ formValid: fieldValidity.empName && fieldValidity.age })
+    }
+    validateSalary = (e) => {
+        let salary = e.target.value;
+        this.setState({ salary: salary })
+    }
+    validateAchievements = (e) => {
+        let achievements = e.target.value;
+        this.setState({ achievements: achievements })
+    }
+    update = (e) => {
+        e.preventDefault();
+        if (this.state.formValid) {
+            var formJSON = {
+                empId: 102,
+                empName: this.state.empName,
+                age: this.state.age,
+                salary: this.state.salary,
+                achievements: this.state.achievements
+            }
+            console.log(JSON.stringify(formJSON));
+            this.setState({ successMessage: JSON.stringify(formJSON) });
+        }
+    }
+    render() {
         return (
-        <div>
-            <h4>UI Courses list</h4>
-            <div> { 
-                users.map((userDetails)=> { 
-                    return (<div key={userDetails.id}>
-                        <b>Course: </b>{userDetails.name} <br/> 
-                        <b>Educator: </b>{userDetails.educator} <br/>
-                        <button onClick={this.clickHandler}>Details</button><br/><br/>
-                    </div>);
-                })}
+            <div style={{width:500, margin:'0px auto'}}>
+                <h3 className="text-center">The selected ID is {102}</h3>
+                <form >
+                    <div className="form-group">
+                        <label>Employee Id:</label>
+                        <input className="form-control" disabled value={102} />
+                    </div>
+                    <div className="form-group">
+                        <label>Name:</label>
+                        <input className="form-control" onChange={this.validateName} value={this.state.empName} />
+                    </div>
+                    <span className="text-danger">{this.state.formErrors.empNameErr}</span>
+                    <div className="form-group">
+                        <label>Age:</label>
+                        <input className="form-control" onChange={this.validateAge} value={this.state.age} />
+                    </div>
+                    <span className="text-danger">{this.state.formErrors.empAgeErr}</span>
+                    <div className="form-group">
+                        <label>Salary:</label>
+                        <select onChange={this.validateSalary} className="form-control" value={this.state.salary}>
+                            <option value="20000">20000</option>
+                            <option value="30000">30000</option>
+                            <option value="40000">40000</option>
+                            <option value="50000">50000</option>
+                        </select>
+                    </div>
+                    <div className="form-group">
+                        <label>Achievements:</label>
+                        <textarea onChange={this.validateAchievements} className="form-control" value={this.state.achievements} />
+                    </div>
+                    <button type="button" onClick={this.update} className="btn btn-success" disabled={!this.state.formValid}>Update</button><br/>
+                    <span className="text-success">{this.state.successMessage}</span>
+                </form>
             </div>
-            <button onClick={this.exitComponent}>Exit</button>
-        </div>
-        );  
-    }
-    componentDidMount = () => {
-        console.log("Mounting Phase: 'componentDidMount()' executed");  
-    }
-    clickHandler = () => {
-        console.log("Details button is clicked, state will change...");
-        this.setState({ selected: true });
-    }
-    exitComponent = () => {
-        console.log("Exit button is clicked, component will be destroyed");
-        ReactDOM.unmountComponentAtNode(document.getElementById('root'));
-    }
-    componentWillUnmount = () => {
-        console.log("Un-Mounting Phase: 'componentWillUnmount()' executed");
+        );
     }
 }
 
